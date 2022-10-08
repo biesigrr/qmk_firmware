3569470546cb496de94555ed6c8f06aab3636e54
Markus Eckert <markus@bie.si>
2022-10-09 01:08:02 +0200

    Update game mode

--- keyboards/moonlander/keymaps/biesigrr/custom.c
+++ keyboards/moonlander/keymaps/biesigrr/custom.c
@@ -1,16 +1,42 @@
 #include QMK_KEYBOARD_H
-#include <stdint.h>
 
-#define GAME_MODE_LAYER 4
+#define GAME_LAYER 1
 
-void housekeeping_task_user(void) {
-    if (!is_transport_connected()) {
-        if (IS_LAYER_OFF(GAME_MODE_LAYER)) {
-            layer_on(GAME_MODE_LAYER);
-        }
-    } else {
-        if (IS_LAYER_ON(GAME_MODE_LAYER)) {
-            layer_off(GAME_MODE_LAYER);
+bool chat_mode_toggled;
+bool automatic_toggle;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (KC_ENTER == keycode && record->event.pressed) {
+        if (IS_LAYER_ON(GAME_LAYER)) {
+            chat_mode_toggled = true;
+            automatic_toggle = true;
+            layer_off(GAME_LAYER);
+            return true;
         }
     }
+
+    if ((KC_ESC == keycode || KC_ENTER == keycode) && record->event.pressed) {
+        if (IS_LAYER_OFF(GAME_LAYER) && chat_mode_toggled) {
+            chat_mode_toggled = false;
+            automatic_toggle = true;
+            layer_on(GAME_LAYER);
+            return true;
+        }
+    }
+
+    return true;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    static bool game_mode_layer_on = false;
+    if (game_mode_layer_on != IS_LAYER_ON_STATE(state, GAME_LAYER)) {
+        if (!automatic_toggle) {
+            chat_mode_toggled = false;
+        }
+
+        automatic_toggle = false;
+        game_mode_layer_on = !game_mode_layer_on;
+    }
+
+    return state;
 }
