6ee6915af656bf8ab7947d946272024cca95db75
Markus Eckert <markus@bie.si>
2024-09-15 21:28:51 +0200

    Update keychron v3 layout

--- keyboards/keychron/v3/ansi/keymaps/biesigrr/rules.mk
+++ keyboards/keychron/v3/ansi/keymaps/biesigrr/rules.mk
@@ -1,6 +1,7 @@
 # Build Options
 #   change yes to no to disable
 #
+AUTO_SHIFT_ENABLE = no
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
@@ -19,5 +20,3 @@ RAW_ENABLE = yes
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
-
-SRC += custom.c
